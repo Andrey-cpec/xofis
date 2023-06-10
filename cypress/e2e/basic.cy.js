720a77fa8048822cbf61c962d72b632b9e39b74a
Andrey <80820392+Andrey-cpec@users.noreply.github.com>
2023-06-10 16:26:03 +0300

    новое

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -5,6 +5,6 @@ describe('sample render test', () => {
 
   it('displays the title text', () => {
     cy.get('h2')
-    .contains('Офисная мебель.Хорошая обстановка');
+    .contains('Хорошая обстановка');
   })
 }) 
